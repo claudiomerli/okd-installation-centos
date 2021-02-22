7a972766132b2ef9bc9d1f1571e448f6f698306c
Claudio Merli <cladlomerli99@gmail.com>
2021-02-22 19:54:39 +0100

    updated ips

--- provisioning/settings.sh
+++ provisioning/settings.sh
@@ -6,22 +6,22 @@
 export OKD_VERSION=3.11
 
 #OKD Master Node Configuration
-export OKD_MASTER_IP=35.193.134.243
+export OKD_MASTER_IP=104.155.144.151
 export OKD_MASTER_HOSTNAME=okd-master-node
 
 #OKD Worker Node 1 Configuration
-export OKD_WORKER_NODE_1_IP=104.155.144.151
+export OKD_WORKER_NODE_1_IP=34.123.66.101
 export OKD_WORKER_NODE_1_HOSTNAME=okd-worker-node-1
 
 #OKD Infra Node 1 Configuration
-export OKD_INFRA_NODE_1_IP=34.123.66.101
+export OKD_INFRA_NODE_1_IP=35.193.134.243
 export OKD_INFRA_NODE_1_HOSTNAME=okd-infra-node-1
 
 #The  below setting will be used to access OKD console https://console.$DOMAIN:$API_PORT"
 #By default we can login using the URL https://console.okd.nip.io:8443
 #To access URL from your local system we need to configure master host in C:\Windows\System32\drivers\etc\hosts file as below
 #100.10.10.100  console.okd.nip.io
-export DOMAIN=35.193.134.243.nip.io
+export DOMAIN=104.155.144.151.nip.io
 export API_PORT=8443
 
 #OKD Login Credentials
